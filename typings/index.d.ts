cda84440499d7ea1f3c890a3045ce4e52d4e5611
Mario <mqs@melix.org>
2017-02-16 14:07:35 +0100

    last commit (don't remember : | )

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -5,6 +5,7 @@
 /// <reference path="globals/ejs/index.d.ts" />
 /// <reference path="globals/express-session/index.d.ts" />
 /// <reference path="globals/express/index.d.ts" />
+/// <reference path="globals/grunt/index.d.ts" />
 /// <reference path="globals/jade/index.d.ts" />
 /// <reference path="globals/mongodb/index.d.ts" />
 /// <reference path="globals/msnodesql/index.d.ts" />
